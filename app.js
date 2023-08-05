a10ce0093ea909b203d6fe38213bffa2dc87e9f1
RachiRaw <131473716+RachiRaw@users.noreply.github.com>
2023-08-05 22:40:55 +0530

    Added dependencies

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const productroutes = require('./routes/product');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://127.0.0.1:27017/EcomDB')
     .then(() => { console.log('EcomDB connected!') })
     .catch((err) => console.log(err));
 
-app.engine('ejs', require('ejs-mate'));
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   res.send('Working Fine!');
